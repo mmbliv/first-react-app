45c1c7f21d45932c05ebccdc6069185cf1728233
min <mmbliv@gmail>
2021-09-16 21:09:25 -0700

    paper data ordered by year

--- src/components/publicationCom/PapersCom.js
+++ src/components/publicationCom/PapersCom.js
@@ -4,36 +4,51 @@ import { paperData } from "../../data/paper-data";
 import { findByAltText } from "@testing-library/react";
 
 export const PapersCom = () => {
-  const orderData = (item) => {
-    const orderedData = {};
-    const dataArray = [];
-    if (orderedData[item.year] === "undefined") {
-      dataArray.push(item);
-      orderedData[item.year] = dataArray;
+  // convert the old array data to an new object data with keys of different year
+
+  const orderData = (paperData) => {
+    let orderedData = {};
+    for (let i = 0; i < paperData.length; i++) {
+      if (orderedData[paperData[i].year] === undefined) {
+        let dataArray = [];
+        dataArray.push(paperData[i]);
+        orderedData[paperData[i].year] = dataArray;
+      } else {
+        orderedData[paperData[i].year].push(paperData[i]);
+      }
     }
+    return orderedData;
   };
+  //   asign new ordered data to newOrderedData
+  const newOrderedDate = orderData(paperData);
+
   return (
     <div>
-      {paperData.map(
-        orderData
-
-        // return (
-        //   <>
-        //     <div>
-        //       <button className="w-full h-5 bg-gray-400 text-gray-800">
-        //         2021
-        //       </button>
-        //     </div>
-        //     <PaperCom
-        //       key={item.id}
-        //       title={item.title}
-        //       author={item.author}
-        //       year={item.year}
-        //       magazine={item.magazine}
-        //     />
-        //   </>
-        // );
-      )}
+      {/* loop through the new object data ; item represent the different year */}
+      {Object.keys(newOrderedDate).map((item, index) => {
+        return (
+          <>
+            <div key={index}>
+              <button className="w-full h-5 bg-gray-400 text-gray-800">
+                {item}
+              </button>
+            </div>
+            {newOrderedDate[item].map((paper) => {
+              return (
+                <>
+                  <PaperCom
+                    key={paper.id}
+                    title={paper.title}
+                    author={paper.author}
+                    year={paper.year}
+                    magazine={paper.magazine}
+                  />
+                </>
+              );
+            })}
+          </>
+        );
+      })}
     </div>
   );
 };
